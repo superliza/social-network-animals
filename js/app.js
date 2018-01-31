b71390cde70162f8b7633b78de38dd56c27462c9
lauralm <mein_herz_ist_kalt@hotmail.com>
2018-01-31 00:50:37 -0600

    editando estructura

--- js/app.js
+++ js/app.js
@@ -48,3 +48,8 @@ function addAccount (){ //Contador de tipeado
       counting.value =("-"+(longitud-max));
     }
 };
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+     
